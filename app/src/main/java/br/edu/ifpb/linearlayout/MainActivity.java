50aebd9e8322f3ea311db7593e68af1caeffd514
EddieKaleb <kaleb.eddie964@gmail.com>
2016-07-24 12:04:58 -0300

    SplashScreen added

--- app/src/main/java/br/edu/ifpb/linearlayout/MainActivity.java
+++ app/src/main/java/br/edu/ifpb/linearlayout/MainActivity.java
@@ -1,7 +1,6 @@
 package br.edu.ifpb.linearlayout;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -10,6 +9,5 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
