ec012af805438b7ad54f250eac269f86e82673f8
Eddie <kaleb.eddie964@gmail.com>
2016-07-26 08:29:53 -0300

    Build Glade

--- app/src/main/java/br/edu/ifpb/linearlayout/SplashScreen.java
+++ app/src/main/java/br/edu/ifpb/linearlayout/SplashScreen.java
@@ -2,8 +2,10 @@ package br.edu.ifpb.linearlayout;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,12 +22,13 @@ public class SplashScreen extends Activity {
             @Override
             public void run() {
                 finish();
-
                 Intent intent = new Intent();
                 intent.setClass(SplashScreen.this, MainActivity.class);
                 intent.setClass(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
             }
         }, 6000);
+
+
     }
 }
