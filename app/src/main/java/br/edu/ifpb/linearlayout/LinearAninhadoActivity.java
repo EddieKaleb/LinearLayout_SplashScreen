44d4486a35b7d5b5fdad55dc99562247833a7e9b
FannyVieira <FannyVieiira@hotmail.com>
2016-07-26 21:16:33 -0300

    - Tela e activity do layout aninhado
    Xml e activit do exemplo do linear layout aninhado

---
+++ app/src/main/java/br/edu/ifpb/linearlayout/LinearAninhadoActivity.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.linearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearAninhadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_aninhado);
+    }
+}
