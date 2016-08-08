cafe467437b36d267dde293879c150e4a24d561f
FannyVieira <FannyVieiira@hotmail.com>
2016-08-08 18:21:01 -0300

    - add layout weight

--- app/src/main/java/br/edu/ifpb/linearlayout/MainActivity.java
+++ app/src/main/java/br/edu/ifpb/linearlayout/MainActivity.java
@@ -77,8 +77,13 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(),"Layout gravity Selected",Toast.LENGTH_SHORT).show();
                         setFragment(R.layout.activity_gravity);
                         return true;
+                    case R.id.weight:
+                        Toast.makeText(getApplicationContext(),"Layout weight Selected",Toast.LENGTH_SHORT).show();
+                        setFragment(R.layout.activity_weight);
+                        return true;
                     default:
                         Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
+                        setFragment(R.drawable.init);
                         return true;
 
                 }
@@ -116,27 +121,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void setFragment(int idLayout){
 
